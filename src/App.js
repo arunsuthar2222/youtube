2ff0c606627c9abcc87263285b49a1c4fa775aa9
Arun Suthar <71078280+arunsuthar2222@users.noreply.github.com>
2023-05-01 18:13:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header/Header";
-import "./app.css";
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Catagory from "./components/Catagory";
